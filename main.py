9e1fc2fb2d2e0de263660dc5923b4da41e7d0974
bg117 <bg117@sooosilly.xyz>
2023-03-10 23:40:01 +0800

    add test function for tokens

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from blast.token import Token
+from blast.tokentype import TokenType
+
 def main():
-    pass
+    token = Token(TokenType.NUMBER, 1)
+
+    print(repr(token))
 
 if __name__ == '__main__':
     main()
