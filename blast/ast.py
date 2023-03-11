02e8e90726c19167d35bcfa8fdc5c639ead85417
bg117 <bg117@sooosilly.xyz>
2023-03-11 20:02:02 +0800

    add binary expression AST

--- blast/ast.py
+++ blast/ast.py
@@ -19,6 +19,25 @@ class ExprAST(AST):
     pass
 
 
+class BinaryExprAST(ExprAST):
+    """AST node representing a binary expression.
+
+    This class is used to represent the abstract syntax tree of a BLAST
+    program.  It is used by the parser and the interpreter.
+    """
+    def __init__(self, op, lhs, rhs):
+        """Initialize a BinaryExprAST.
+
+        Args:
+            op (Token): The operator.
+            lhs (ExprAST): The left-hand side expression.
+            rhs (ExprAST): The right-hand side expression.
+        """
+        self.op = op
+        self.lhs = lhs
+        self.rhs = rhs
+
+
 class NumberExprAST(ExprAST):
     """AST node representing a number.
 
