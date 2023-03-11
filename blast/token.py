28651df47990ccf22caf2746eefd5ef202dc331c
bg117 <bg117@sooosilly.xyz>
2023-03-11 17:48:35 +0800

    add docstring to token.py classes

--- blast/token.py
+++ blast/token.py
@@ -2,6 +2,12 @@ from enum import Enum
 
 
 class Token:
+    """Class representing a token.
+    
+    A token is a lexical unit of a programming language. It is a sequence of characters
+    that have a meaning in the programming language. For example, the token '1' is a
+    NUMBER token, and the token '+' is a PLUS token.
+    """
     def __init__(self, type, lexeme):
         self.type = type
         self.lexeme = lexeme
@@ -20,6 +26,8 @@ class Token:
 
 
 class TokenType(Enum):
+    """Enum representing the different types of tokens.
+    """
     EOF = -1
 
     NUMBER = 0
