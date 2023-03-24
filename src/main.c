2807c43d5c638f1c7d8bbb48c969f9a8c4f2a03c
bg117 <bg117@sooosilly.xyz>
2023-03-24 23:35:07 +0800

    test scanner

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
-int main(int argc, char **argv) {
-    return 0;
+#include "scanner.h"
+#include "token.h"
+
+int main(int argc, char **argv)
+{
+    char          *src     = "1 + 2 * 3";
+    struct scanner scanner = { .src = src };
+
+    int           num_tokens;
+    struct token *tokens = scanner_scan(&scanner, &num_tokens);
 }
