5992ae00adfd5256058190462a9f292dce8cf5e1
bg117 <bg117@sooosilly.xyz>
2023-03-25 13:43:18 +0800

    print tokens

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "scanner.h"
 #include "token.h"
 
+#include <stdio.h>
+
 int main(int argc, char **argv)
 {
     char          *src     = "1 + 2 * 3";
@@ -8,4 +10,7 @@ int main(int argc, char **argv)
 
     int           num_tokens;
     struct token *tokens = scanner_scan(&scanner, &num_tokens);
+
+    for (int i = 0; i < num_tokens; i++)
+        printf("%d: %s\n", tokens[i].type, tokens[i].lexeme);
 }
