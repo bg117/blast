d1a21891a74dc1c49f50f0553fc052eb1d31fd75
bg117 <bg117@sooosilly.xyz>
2023-03-25 15:51:18 +0800

    implement AST_NUMBER printer

--- src/ast_printer.c
+++ src/ast_printer.c
@@ -5,6 +5,12 @@
 #include "ast.h"
 #include "token.h"
 
+static void visit_expr_number(struct ast *ast, int depth)
+{
+    printf("%*s%f", depth * 2, " ", ast->expr.number.value);
+}
+
 void ast_printer(struct ast *ast)
 {
+
 }
