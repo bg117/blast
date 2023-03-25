58700185d169a0de5fbe9dfe15d3e19be507e510
bg117 <bg117@sooosilly.xyz>
2023-03-25 15:48:26 +0800

    ast_printer boilerplate

---
+++ src/ast_printer.c
@@ -0,0 +1,10 @@
+#include "ast_printer.h"
+
+#include <stdio.h>
+
+#include "ast.h"
+#include "token.h"
+
+void ast_printer(struct ast *ast)
+{
+}
---
+++ src/ast_printer.h
@@ -0,0 +1,13 @@
+#ifndef AST_PRINTER_H
+#define AST_PRINTER_H
+
+struct ast;
+
+/**
+ * @brief Prints an AST.
+ *
+ * @param ast The AST to print.
+ */
+void ast_printer(struct ast *ast);
+
+#endif
