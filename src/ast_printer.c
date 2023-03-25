eee5123ad13f8fde9eaa119443f133c4e8824105
bg117 <bg117@sooosilly.xyz>
2023-03-25 16:00:41 +0800

    dispatch visit functions

--- src/ast_printer.c
+++ src/ast_printer.c
@@ -44,7 +44,32 @@ static void visit_expr_call(struct ast *ast, int depth)
     printf("%*s()", depth * 2, " "); // TODO: print nodes
 }
 
+static void visit(struct ast *ast, int depth)
+{
+    switch (ast->type)
+    {
+    case AST_EXPR_NUMBER:
+        visit_expr_number(ast, depth);
+        break;
+    case AST_EXPR_STRING:
+        visit_expr_string(ast, depth);
+        break;
+    case AST_EXPR_VARIABLE:
+        visit_expr_variable(ast, depth);
+        break;
+    case AST_EXPR_BINARY:
+        visit_expr_binary(ast, depth);
+        break;
+    case AST_EXPR_UNARY:
+        visit_expr_unary(ast, depth);
+        break;
+    case AST_EXPR_CALL:
+        visit_expr_call(ast, depth);
+        break;
+    }
+}
+
 void ast_printer(struct ast *ast)
 {
-
+    visit(ast, 0);
 }
