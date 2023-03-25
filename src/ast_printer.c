4fc1af6b25c102633a983e44b952583565aebf2e
bg117 <bg117@sooosilly.xyz>
2023-03-25 15:51:38 +0800

    implement AST_STRING printer

--- src/ast_printer.c
+++ src/ast_printer.c
@@ -10,6 +10,11 @@ static void visit_expr_number(struct ast *ast, int depth)
     printf("%*s%f", depth * 2, " ", ast->expr.number.value);
 }
 
+static void visit_expr_string(struct ast *ast, int depth)
+{
+    printf("%*s\"%s\"", depth * 2, " ", ast->expr.string.value);
+}
+
 void ast_printer(struct ast *ast)
 {
 
